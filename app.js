27455cf033e7814648e9bba488dea7497229471f
ant2288 <1248207321@qq.com>
2018-03-25 14:34:34 +0800

    修复
    修复一些问题

--- app.js
+++ app.js
@@ -1,10 +1,71 @@
-var util = require('util/util')
+const loginUrl = require('config').loginUrl
+const tokenValidUrl = require('config').tokenValidUrl
 App({
-
+  getUserInfo: function (cb) {
+    var that = this
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    } else {
+      //调用登陆接口
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      })
+    }
+  },
   /**
-   * 全局数据
+   * 登陆，如果没有信息就直接注册
    */
-  globalData:{
-    hasLogin:false
+  login:function(){
+    var that = this;
+    var token = that.globalData.token;
+    //验证token
+    if(this.tokenValid()){
+      return;
+    }
+    wx.login({
+      success:data =>{
+        console.log("code为："+data.code);
+        wx.request({
+          url: loginUrl,
+          data:
+            data.code
+          ,
+          method:'POST',
+          success:mydata =>{
+            if(mydata.data.msg == '登陆'){
+              console.log(mydata.data.token)
+              that.globalData.token = mydata.data.token;
+            }
+          }
+        })
+      }
+    })
+  },
+  tokenValid:function(){
+    var that = this;
+    if (that.globalData.token == null) return false;
+    wx.request({
+      url: tokenValidUrl,
+      data: that.globalData.token,
+      method:'POST',
+      success:data =>{
+        if(data.data.msg=='有效'){
+          return true;
+        }else{
+          return false;
+        }
+      }
+    })
+  },
+  globalData: {
+    userInfo: null,
+    token:null
   }
 })
--- config.js
+++ config.js
@@ -7,8 +7,10 @@ var host = '127.0.0.1:8080'
 var config = {
   host,
   //登陆地址
-  loginUrl: `http://${host}/login`,
+  loginUrl: `http://${host}/wx/login`,
   //上传用户信息地址
-  sendUserInfoUrl: `http://${host}/sendUserInfo`
+  sendUserInfoUrl: `http://${host}/sendUserInfo`,
+  //验证token是否有效
+  tokenValidUrl: `http://${host}/wx/tokenValid`
 };
 module.exports = config
